678be483cace5e46ae3f5c72b050bf94658df80d
Wenjun Zhang <wenjunz@alice.mit.edu>
2015-09-01 18:12:26 -0400

    add reverse lined list, following jiuzhang.com

---
+++ reverse_linked_list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curt = None;
+        while head is not None:
+            temp = head.next;
+            head.next = curt;
+            curt = head;
+            head = temp;
+        
+        return curt
