9f7468662b4535b492758fd619913fcf916757c6
Wenjun Zhang <wenjunz@alice.mit.edu>
2015-09-02 12:58:52 -0400

    added linked list cycle, beat 100.00% python coders

---
+++ linked_list_cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None: return False;
+        fast, slow = head, head;
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next;
+            slow = slow.next;
+            if fast is slow: return True
+        return False
